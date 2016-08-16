78da5be6ae483049ece82a00b4ea42cf8567d111
zmaster587 <zmasterfun@gmail.com>
2016-08-16 02:16:54 -0400

    changelog

--- AdvancedRocketry/index.php
+++ AdvancedRocketry/index.php
@@ -11,7 +11,21 @@ player specified planets to the game.&nbsp; These planets each have
 unique properties such as Atmosphere Density, Distance from the sun,
 Atmosphere composition, Average Temperature, and size.&nbsp; Players
 can build rockets out of blocks to travel to these other worlds.<br />
-</p>';
+</p>
+<hr>
+<h2>What\'s New?</h2>
+<h3>0.6.8</h3>
+<ul>
+<li>Fixed Rockets being stuck in the air on space stations</li>
+<li>Fixed O2 detectors having the wrong name</li>
+<li>Fixed low gravity being missing in space</li>
+<li>Added <a href="./blocks/LaserDrill.php">Laser Drill</a></li>
+<li>Added <a href="./blocks/MicrowaveReciever.php">Microwave Reciever</a></li>
+<li>Overhauled textures for planets as viewed from space stations</li>
+<li>Fixed warp not working half the time in single player</li>
+<li>Fixed planet information not being displayed in the planet selector interface</li>
+<li>Actual documentation for some of the mod on this site</li>
+</ul>';
     
     $infoBarContent = 'infoBarContent';
     include($_SERVER["DOCUMENT_ROOT"].'/AdvancedRocketry/template.php');

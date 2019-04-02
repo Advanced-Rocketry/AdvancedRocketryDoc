1894c015d1b2380748b823e5d01511ac0f61f955
jlaw21 <jlaw@warren.k12.in.us>
2019-04-01 20:27:22 -0400

    Converting to new format

--- AdvancedRocketry/Templates/Template.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $title = 'NAME OF BLOCK';
-    
-    $mainContent = '<h1>'.$title.'</h1><hr><img align="left" src="../img/nameOfBlock_demo.jpg" /><p><h3>Overview</h3>INSERT TEXT HERE</p>';
-    
-    $infoBarContent = $title.'<hr><img src="../img/nameOfBlock_block.png" /><hr>Hardness: 2<br />Blast Resistance: 0<br />Requires Tool: Yes<br />Full Block: No<br />Sealable: No<br />Tile Entity: Yes';
-    include($_SERVER["DOCUMENT_ROOT"].'/AdvancedRocketry/template.php');
-?> 
--- AdvancedRocketry/items/datachip.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $title = 'Data Storage Unit';
-    
-    $mainContent = '<h1>'.$title.'</h1><hr><h2><center>Overview</center></h2>
-    <p>The data storage unit is used to store data of different types.  This is useful for transporting data from a <a href="../blocks/satelliteTerminal.php">Satellite Terminal</a> or Observetory to machines like the <a href="../blocks/Abdp.php">Astrobody Data Processor</a>.</p>';
-    
-    $infoBarContent = $title.'<hr><img height=128px style="image-rendering: -webkit-optimize-contrast; image-rendering: -moz-crisp-edges; image-rendering: -o-crisp-edges; image-rendering: crisp-edges;" src="../img/items/dataStorageUnit.png" /><hr>Has NBT: Yes<hr><center><h3>Teir 1</h3></center>Data Storage capacity: 500';
-    include($_SERVER["DOCUMENT_ROOT"].'/AdvancedRocketry/template.php');
-?> 
